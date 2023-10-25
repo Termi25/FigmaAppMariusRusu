81789d5a8ce30eaf8e4c2f83c727c56ed1c22eaa
Marius Rusu <mariusrusu208@gmail.com>
2023-10-25 18:28:08 +0300

    Initial commit

---
+++ app/src/main/java/com/ase/figmaappmariusrusu/android_large___1_activity.java
@@ -0,0 +1,90 @@
+package com.ase.figmaappmariusrusu;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___1
+	 *	@date 		Wednesday 25th of October 2023 02:47:59 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class android_large___1_activity extends Activity {
+
+	
+	private View _bg__android_large___1_ek2;
+	private View background;
+	private View menu2;
+	private ImageView profilepic;
+	private View menu3;
+	private View _bg__menu1_ek1;
+	private View background_menu1;
+	private TextView color_galore;
+	private View _bg__colorsection_ek1;
+	private View colormap;
+	private View transparency;
+	private View _bg__hex_ek1;
+	private TextView hex_ek2;
+	private View texthex;
+	private View _bg__rgb_ek1;
+	private TextView rgb_ek2;
+	private View textrgb;
+	private View _bg__hsb_ek1;
+	private TextView hsb_ek2;
+	private View texthsb;
+	private View _bg__cmyk_ek1;
+	private TextView cmyk_ek2;
+	private View textcmyk;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___1);
+
+		
+		_bg__android_large___1_ek2 = (View) findViewById(R.id._bg__android_large___1_ek2);
+		background = (View) findViewById(R.id.background);
+		menu2 = (View) findViewById(R.id.menu2);
+		profilepic = (ImageView) findViewById(R.id.profilepic);
+		menu3 = (View) findViewById(R.id.menu3);
+		_bg__menu1_ek1 = (View) findViewById(R.id._bg__menu1_ek1);
+		background_menu1 = (View) findViewById(R.id.background_menu1);
+		color_galore = (TextView) findViewById(R.id.color_galore);
+		colormap = (View) findViewById(R.id.colormap);
+		transparency = (View) findViewById(R.id.transparency);
+		_bg__hex_ek1 = (View) findViewById(R.id._bg__hex_ek1);
+		hex_ek2 = (TextView) findViewById(R.id.hex_ek2);
+		texthex = (View) findViewById(R.id.texthex);
+		_bg__rgb_ek1 = (View) findViewById(R.id._bg__rgb_ek1);
+		rgb_ek2 = (TextView) findViewById(R.id.rgb_ek2);
+		textrgb = (View) findViewById(R.id.textrgb);
+		_bg__hsb_ek1 = (View) findViewById(R.id._bg__hsb_ek1);
+		hsb_ek2 = (TextView) findViewById(R.id.hsb_ek2);
+		texthsb = (View) findViewById(R.id.texthsb);
+		_bg__cmyk_ek1 = (View) findViewById(R.id._bg__cmyk_ek1);
+		cmyk_ek2 = (TextView) findViewById(R.id.cmyk_ek2);
+		textcmyk = (View) findViewById(R.id.textcmyk);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
